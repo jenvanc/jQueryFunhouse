dee20fb5495202f0f8f51e7958ff4a193122d115
Jenna Van Conett <jenvanc@gmail.com>
2018-02-06 14:51:39 -0500

    Change away-team to pokemon reference

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript File
+$( document ).ready(function() {
+
+$("#garage-door-opener").click(function() {
+    $("#garage-door").slideToggle();
+});
+
+$("#pokeball").click(function() {
+    $("#pikachu").fadeIn();
+});
+
+$("#light-switch").click(function() {
+    $("#light-bulb").toggle();
+});
+});
